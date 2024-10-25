5e594471198e52f9883ab6a5be28e3904c9c8bae
Viswanathan-Ganesh <152311052+Viswanathan-Ganesh@users.noreply.github.com>
2024-10-26 00:40:40 +0530

    Noice

--- Assets/Flying.cs
+++ Assets/Flying.cs
@@ -11,6 +11,11 @@ public class Flying : MonoBehaviour
     public float g;
     public Rigidbody rb;
     public Vector3 initialForce;
+    public float radius;
+    float timer = 0;
+    float end = 0.2f;
+    bool timerStart = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +25,17 @@ public class Flying : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Mul(gameObject.transform.forward, 0.48 * 0.37 * PressureFinder(WindVelocity, transform.position.y)));
+        rb.AddForce(Mul(gameObject.transform.up, 0.48 * 1.6 * Mathf.Abs((float)(PressureFinder(WindVelocity, transform.position.y - radius) - PressureFinder(rb.velocity, transform.position.y)))));
+        if (timerStart)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer > end)
+        {
+            Debug.Log("End");
+            Destroy(gameObject.GetComponent<Flying>());
+        }
     }
     double PressureFinder(Vector3 velocity, float height)
     {
@@ -30,5 +45,15 @@ public class Flying : MonoBehaviour
     Vector3 Mul(Vector3 v, double m)
     {
         return new Vector3(v.x * ((float)m), v.y * ((float)m), v.z * ((float)m));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision != null)
+        {
+            timerStart = true;
+            Debug.Log("COllision");
+            
+        }
     }
 }
